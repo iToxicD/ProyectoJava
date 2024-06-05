03c9afa998be11cd79d4f2998b4a504d38b9b1d9
Álvaro <alvarocc948@gmail.com>
2024-06-05 19:03:07 +0200

    update main

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     }
 
-    //Método para añadir canciones
+    //Metodo para aï¿½adir canciones
     public static void nuevaCancion(){
         Scanner sc = new Scanner(System.in);
         String sql = "INSERT INTO playlist (id, nombre, autor, duracion) VALUES (?,?,?,?);";
@@ -29,19 +29,19 @@ public class Main {
         Time duracion = null;
         int actualizarFila;
         try {
-            //Conexión con la base de datos
+            //Conexiï¿½n con la base de datos
             Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/spotify", "root", "123456");
             PreparedStatement statement = conexion.prepareStatement(sql);
-            System.out.println("Añade id de canción: ");
+            System.out.println("Aï¿½ade id de canciï¿½n: ");
             id = sc.nextInt();
             statement.setInt(1, id);
-            System.out.println("Añade nombre de canción: ");
+            System.out.println("Aï¿½ade nombre de canciï¿½n: ");
             nombre = sc.next();
             statement.setString(2, nombre);
-            System.out.println("Añade autor de canción: ");
+            System.out.println("Aï¿½ade autor de canciï¿½n: ");
             autor = sc.next();
             statement.setString(3, autor);
-            System.out.println("Añade duración de canción: ");
+            System.out.println("Aï¿½ade duraciï¿½n de canciï¿½n: ");
             duracion = Time.valueOf(sc.next());
             statement.setTime(4, duracion);
             actualizarFila = statement.executeUpdate();
@@ -52,14 +52,14 @@ public class Main {
         }
     }
 
-    // Método para mostrar la lista de canciones.
+    // Mï¿½todo para mostrar la lista de canciones.
     public static void listaCanciones(){
         String listaSql = "SELECT * FROM playlist;";
         int id;
         String nombre, autor;
         Time duracion;
         try {
-            //Conexión con la base de datos
+            //Conexiï¿½n con la base de datos
             Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/spotify", "root", "123456");
             Statement statement = conexion.createStatement();
             ResultSet rs = statement.executeQuery(listaSql);
@@ -68,7 +68,7 @@ public class Main {
                 nombre = rs.getString("nombre");
                 autor = rs.getString("autor");
                 duracion = rs.getTime("duracion");
-                System.out.println("Id: " + id +", Nombre: " + nombre + ", Autor: " + autor + ", Duración: " + duracion );
+                System.out.println("Id: " + id +", Nombre: " + nombre + ", Autor: " + autor + ", Duraciï¿½n: " + duracion );
             }
             conexion.close();
             statement.close();
@@ -76,17 +76,17 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
-    // Método para borrar canciones
+    // Metodo para borrar canciones
     public static void borrarCancion(){
         Scanner sc = new Scanner(System.in);
         String borrarSql = "DELETE FROM playlist WHERE id = ?;";
         int id = 0;
         int actualizarFila;
         try {
-            //Conexión con la base de datos
+            //Conexiï¿½n con la base de datos
             Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/spotify", "root", "123456");
             PreparedStatement statement = conexion.prepareStatement(borrarSql);
-            System.out.println("Id de la canción a eliminar: ");
+            System.out.println("Id de la canciï¿½n a eliminar: ");
             id = sc.nextInt();
             statement.setInt(1, id);
             actualizarFila = statement.executeUpdate();
