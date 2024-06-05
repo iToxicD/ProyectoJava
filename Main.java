b4de86515a2ccfa1820cee662aeebfcd8aa5bde3
Francisco Flores Prieto <pacofloresprieto@gmail.com>
2024-06-05 18:57:11 +0200

    Actualizacion del Main
    Comentarios y cierre de la base de datos

--- Main.java
+++ Main.java
@@ -11,13 +11,16 @@ public class Main {
                 nuevaCancion();
                 break;
             case 2:
+                listaCanciones();
+                break;
+            case 3:
                 borrarCancion();
                 break;
         }
 
     }
 
-    //Metodo para aÃ±adir canciones
+    //Método para añadir canciones
     public static void nuevaCancion(){
         Scanner sc = new Scanner(System.in);
         String sql = "INSERT INTO playlist (id, nombre, autor, duracion) VALUES (?,?,?,?);";
@@ -26,33 +29,37 @@ public class Main {
         Time duracion = null;
         int actualizarFila;
         try {
+            //Conexión con la base de datos
             Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/spotify", "root", "123456");
             PreparedStatement statement = conexion.prepareStatement(sql);
-            System.out.println("AÃ±ade id de cancion: ");
+            System.out.println("Añade id de canción: ");
             id = sc.nextInt();
             statement.setInt(1, id);
-            System.out.println("AÃ±ade nombre de cancion: ");
+            System.out.println("Añade nombre de canción: ");
             nombre = sc.next();
             statement.setString(2, nombre);
-            System.out.println("AÃ±ade autor de cancion: ");
+            System.out.println("Añade autor de canción: ");
             autor = sc.next();
             statement.setString(3, autor);
-            System.out.println("AÃ±ade duracion de cancion: ");
+            System.out.println("Añade duración de canción: ");
             duracion = Time.valueOf(sc.next());
             statement.setTime(4, duracion);
             actualizarFila = statement.executeUpdate();
+            conexion.close();
+            statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    // MÃ©todo para mostrar la lista de canciones.
+    // Método para mostrar la lista de canciones.
     public static void listaCanciones(){
         String listaSql = "SELECT * FROM playlist;";
         int id;
         String nombre, autor;
         Time duracion;
         try {
+            //Conexión con la base de datos
             Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/spotify", "root", "123456");
             Statement statement = conexion.createStatement();
             ResultSet rs = statement.executeQuery(listaSql);
@@ -61,28 +68,34 @@ public class Main {
                 nombre = rs.getString("nombre");
                 autor = rs.getString("autor");
                 duracion = rs.getTime("duracion");
-                System.out.println("Id: " + id +", Nombre: " + nombre + ", Autor: " + autor + ", DuraciÃ³n: " + duracion );
+                System.out.println("Id: " + id +", Nombre: " + nombre + ", Autor: " + autor + ", Duración: " + duracion );
             }
+            conexion.close();
+            statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    // MÃ©todo para borrar canciones
+    // Método para borrar canciones
     public static void borrarCancion(){
         Scanner sc = new Scanner(System.in);
         String borrarSql = "DELETE FROM playlist WHERE id = ?;";
         int id = 0;
         int actualizarFila;
         try {
+            //Conexión con la base de datos
             Connection conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/spotify", "root", "123456");
             PreparedStatement statement = conexion.prepareStatement(borrarSql);
-            System.out.println("Id de la cancion a eliminar: ");
+            System.out.println("Id de la canción a eliminar: ");
             id = sc.nextInt();
             statement.setInt(1, id);
             actualizarFila = statement.executeUpdate();
+            conexion.close();
+            statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+            
+        }
         }
 
     }
-}
